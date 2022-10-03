ec411d27821dc1b47cc5051b6b9c4e2d3bc9c5df
Stefan Bürk <stefan@buerk.tech>
2022-10-03 17:12:10 +0200

    [FEATURE] Implement plugin handling code for symlinking packages
    This change adds the basic implementation to:
    * symlink extension root-path to `vendor/<package-name>`
    * symlink core extensions from `vendor/typo3/cms-<extension-key>` to
      `<web-dir>/typo3/sysext/<extension-key>`
    * symlink extension from `vendor/<package-name>` to
      `<web-dir>/typo3conf/ext/<extension-key>`

--- src/Installer/Plugin.php
+++ src/Installer/Plugin.php
@@ -15,6 +15,43 @@ declare(strict_types=1);
 
 namespace SBUERK\TYPO3CmsComposerInstallersTestingFrameworkBridge\Installer;
 
-class Plugin
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+use SBUERK\TYPO3CmsComposerInstallersTestingFrameworkBridge\Services\PluginService;
+
+class Plugin implements PluginInterface, EventSubscriberInterface
 {
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+        // noop
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+        // noop
+    }
+
+    /**
+     * @return array<string, string|array{0: string, 1?: int}|array<array{0: string, 1?: int}>>
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ScriptEvents::POST_AUTOLOAD_DUMP => ['postAutoloadDump', -1],
+        ];
+    }
+
+    public function postAutoloadDump(Event $event): void
+    {
+        (new PluginService($event->getComposer(), $event->getIO()))
+            ->handle();
+    }
 }
